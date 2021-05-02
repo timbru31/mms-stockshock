9c1eb5e360b0f0067f92af7f62688f4b53e05417
Tim Brust <github@timbrust.de>
2021-05-03 00:13:31 +0200

    fix: increase Saturn timeout again

--- src/models/stores/saturn.ts
+++ src/models/stores/saturn.ts
@@ -2,6 +2,9 @@ import { CommonStore } from "./abstract-store";
 import { Store } from "./store";
 
 export class Saturn extends CommonStore implements Store {
+    MIN_SLEEP_TIME = 750;
+    MAX_SLEEP_TIME = 2500;
+
     readonly baseUrl = "https://www.saturn.de";
     readonly countryCode = "DE";
     readonly salesLine = "Saturn";
