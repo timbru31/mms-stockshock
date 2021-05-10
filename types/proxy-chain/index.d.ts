8888fc356d35ac17a2f47cca0c1497f3ee09cd70
Tim Brust <github@timbrust.de>
2021-05-10 18:08:54 +0200

    fix: adds missing types

---
+++ types/proxy-chain/index.d.ts
@@ -0,0 +1,36 @@
+/// <reference types="node" />
+
+declare module "proxy-chain" {
+    import { IncomingMessage } from "http";
+    import { EventEmitter } from "events";
+
+    interface ServerOptions {
+        port?: number;
+        verbose?: boolean;
+        prepareRequestFunction?: (req: RequestFunction) => RequestFunctionResponse;
+    }
+
+    interface RequestFunction {
+        request: IncomingMessage;
+        username: string;
+        password: string;
+        hostname: string;
+        port: number;
+        isHttp: boolean;
+        connectionId: string;
+    }
+
+    interface RequestFunctionResponse {
+        requestAuthentication: boolean;
+        upstreamProxyUrl: string;
+        failMsg?: string;
+    }
+
+    class Server extends EventEmitter {
+        port: number;
+        constructor(options: ServerOptions);
+
+        listen(callback?: () => void): Promise<void>;
+        close(destroyConnections: boolean, callback?: () => void): Promise<void>;
+    }
+}
