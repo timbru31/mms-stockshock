6a5c5f9d541a61e6d3e8158e61f8df986feddf9b
Tim Brust <github@timbrust.de>
2022-02-07 10:11:41 +0000

    fix: bumps graphql version

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,7 +5,7 @@ import type { Logger } from "winston";
 import { createLogger as createWinstonLogger, format, transports } from "winston";
 import type { ConfigModel } from "../models/stores/config-model";
 
-export const GRAPHQL_CLIENT_VERSION = "1.65.0";
+export const GRAPHQL_CLIENT_VERSION = "1.66.0";
 
 function getEmojiForLevel(level: string) {
     switch (colors.stripColors(level)) {
