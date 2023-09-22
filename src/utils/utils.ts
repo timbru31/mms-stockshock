f72db222b83eb895d1deb1535b60dc2a726ae48e
Tim Brust <github@timbrust.de>
2023-09-22 08:27:18 +0000

    fix: update graphql version

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -5,7 +5,7 @@ import type { Logger } from "winston";
 import { createLogger as createWinstonLogger, format, transports } from "winston";
 import type { ConfigModel } from "../models/stores/config-model";
 
-export const GRAPHQL_CLIENT_VERSION = "8.15.0";
+export const GRAPHQL_CLIENT_VERSION = "8.18.0";
 
 function getEmojiForLevel(level: string) {
     switch (colors.stripColors(level)) {
