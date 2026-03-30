d7f52617401b07bf040a97a75918d27b76abec37
Tim Brust <github@timbrust.de>
2026-03-30 22:46:34 +0200

    fix: build

--- src/models/api/selected-product-response.ts
+++
@@ -1,6 +0,0 @@
-import type { Item } from "./product-aggregate";
-import type { Response } from "./response";
-
-export interface SelectedProductResponse extends Response {
-    data?: Item;
-}
--- src/models/notifier.ts
+++ src/models/notifier.ts
@@ -1,11 +1,11 @@
-import type { Item } from "./api/product-aggregate";
+import type { CofrProductAggregate } from "./api/product-aggregate";
 import type { Product } from "./api/product";
 
 export interface Notifier {
     notifyAdmin: (message: string, error?: unknown) => Promise<void>;
     notifyRateLimit: (seconds: number) => Promise<void>;
     notifyCookies: (product: Product, cookies: string[]) => Promise<void>;
-    notifyStock: (item: Item, cookiesAmount?: number) => Promise<void>;
-    notifyPriceChange: (item: Item, oldPrice?: number) => Promise<void>;
+    notifyStock: (item: CofrProductAggregate, cookiesAmount?: number) => Promise<void>;
+    notifyPriceChange: (item: CofrProductAggregate, oldPrice?: number) => Promise<void>;
     shutdown: () => void;
 }
